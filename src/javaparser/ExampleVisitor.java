5a518dfa18f62ae3e07944accea1e1ef7f297c64
babyadoresorange <quochao.tran.up@gmail.com>
2020-03-05 16:46:35 -0500

    clean code. TODO: begin UML

--- src/javaparser/ExampleVisitor.java
+++ src/javaparser/ExampleVisitor.java
@@ -1,80 +1,37 @@
 package javaparser;
 
-import java.util.*;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-public class ExampleVisitor extends AbstractVisitor
-{
-	static int ID = 0;
-	private String filename;
-	private String current_class;
-	private String current_method;
-	private FileOutputStream oFile;
+public class ExampleVisitor extends AbstractVisitor {
+	// private String filename;
+	public DotHandler dot_handler_instance = DotHandler.getInstance();
 
 	public ExampleVisitor(String filename) {
-		this.filename = filename;
-
-		try {
-			File yourFile = new File("./result.csv");
-			yourFile.createNewFile(); // if file already exists will do nothing 
-			this.oFile = new FileOutputStream(yourFile, true); 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// this.filename = filename;
+		dot_handler_instance.init();
 	}
 
-	class Metric {
-		public int if_stmt = 0;
-		public int vars = 0;
-	}
-	private HashMap<String, HashMap<String, Metric>> result = new HashMap<String, HashMap<String, Metric>>();
-
-	public Object visit(CompilationUnit node, Object data){
-		propagate(node, data);
-		
-		try { 
-			for (String c : result.keySet()) {
-    			for (String m : result.get(c).keySet()){
-					oFile.write((ID + ";" + this.filename + ";" + c + ";" + m + ";" + result.get(c).get(m).if_stmt + ";" + result.get(c).get(m).vars + "\n").getBytes());
-          ID++;
-        }
-			}
-			oFile.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return data;
-	}
-
-	public Object visit(ClassDeclaration node, Object data){
-		current_class = ((SimpleNode) node.jjtGetChild(0).jjtGetChild(0)).jjtGetFirstToken().image;
-		propagate(node, data);
-		current_class = "";
-		return data;
-	}
-
-	public Object visit(MethodOrFieldDecl node, Object data){
-		current_method = ((SimpleNode) node.jjtGetChild(1)).jjtGetFirstToken().image;
-		HashMap<String, Metric> temp = new HashMap<String, Metric>();
-		temp.put(current_method, new Metric());
-		result.put(current_class, temp);
-		propagate(node, data);
-		current_method = "";
-		return data;
-	}
-
-	public Object visit(IfStatement node, Object data){
-		result.get(current_class).get(current_method).if_stmt++;
+	// TODO: Read theory about UML
+	public Object visit(CompilationUnit node, Object data) {
 		propagate(node, data);
 		return data;
 	}
 
-  public Object visit(LocalVariableDeclarationStatement node, Object data){
-		result.get(current_class).get(current_method).vars++;
-    propagate(node, data);
+	public Object visit(ClassDeclaration node, Object data) {
+		propagate(node, data);
+		return data;
+	}
+
+	public Object visit(MethodOrFieldDecl node, Object data) {
+		propagate(node, data);
+		return data;
+	}
+
+	public Object visit(IfStatement node, Object data) {
+		propagate(node, data);
+		return data;
+	}
+
+	public Object visit(LocalVariableDeclarationStatement node, Object data) {
+		propagate(node, data);
 		return data;
 	}
 
