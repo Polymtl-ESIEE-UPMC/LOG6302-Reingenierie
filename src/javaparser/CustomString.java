e0e831c61a0539ecef55dc05e9cf75bdfc5abbac
babyadoresorange <quochao.tran.up@gmail.com>
2020-03-05 16:13:14 -0500

    create DotHandler and CustomString

--- src/javaparser/CustomString.java
+++ src/javaparser/CustomString.java
@@ -1,5 +1,31 @@
 package javaparser;
 
 public class CustomString {
+  private String str;
 
+  public CustomString(final String str) {
+    this.str = str;
+  }
+
+  public static CustomString makeCustomString(final String str) {
+    return new CustomString(str);
+  }
+
+  public CustomString indent(int n) {
+    if (n < 1)
+      n = 1;
+    for (int i = 0; i < n; i++) {
+      this.str = " " + this.str;
+    }
+    return this;
+  }
+
+  public CustomString indent() {
+    this.indent(1);
+    return this;
+  }
+
+  public String finish() {
+    return this.str;
+  }
 }
